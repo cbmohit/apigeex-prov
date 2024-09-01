c87a09ce1b55a2c3d8ff167abffb330231c7308d
cbmohit <cbmohit@gmail.com>
2024-09-01 19:09:23 +0530

    api commit

--- main.tf
+++ main.tf
@@ -4,27 +4,8 @@ provider "google" {
   region  = var.region
 }
 
-resource "google_compute_network" "apigee_network" {
-  name       = "apigee-network"
-}
-
-resource "google_compute_global_address" "apigee_range" {
-  name          = "apigee-range"
-  purpose       = "VPC_PEERING"
-  address_type  = "INTERNAL"
-  prefix_length = 16
-  network       = google_compute_network.apigee_network.id
-}
-
-resource "google_service_networking_connection" "apigee_vpc_connection" {
-  network                 = google_compute_network.apigee_network.id
-  service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.apigee_range.name]
-}
-
-resource "google_apigee_organization" "my-test-org" {
-  analytics_region   = "us-central1"
-  project_id         = var.project_id
-  authorized_network = google_compute_network.apigee_network.id
-  depends_on         = [google_service_networking_connection.apigee_vpc_connection]
+# Enable the Compute Engine API
+resource "google_project_service" "compute" {
+  project = var.project_id
+  service = "compute.googleapis.com"
 }
