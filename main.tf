03cadd89ea8648293744e79bb58c367bf3623701
cbmohit <cbmohit@gmail.com>
2024-09-01 22:49:24 +0530

    api2 commit

--- main.tf
+++ main.tf
@@ -4,6 +4,13 @@ provider "google" {
   region  = var.region
 }
 
+# Enable the Cloud Resource Manager API
+resource "google_project_service" "gcp_resource_manager_api" {
+  project = var.project_id
+  service = "cloudresourcemanager.googleapis.com"
+}
+
+
 # This is to Enable the Compute Engine API
 resource "google_project_service" "compute" {
   project = var.project_id
