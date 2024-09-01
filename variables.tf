83f14eaa7209e4e2be296d7fa6d3a30935a777e1
cbmohit <cbmohit@gmail.com>
2024-09-01 18:55:06 +0530

    4commit

--- variables.tf
+++ variables.tf
@@ -3,6 +3,11 @@ variable "GOOGLE_CREDENTIALS" {
     description = "JSON GCP credentials"
 }
 
+variable "TF_CLI_ARGS_plan" {
+  type = string
+  default = ""
+}
+
 variable "project_id" {
     description = "GCP project id"
     type = string
