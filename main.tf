e09d87354eead218f1630da8a22c2294af044891
cbmohit <cbmohit@gmail.com>
2024-09-01 19:02:33 +0530

    6commit

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "google_service_networking_connection" "apigee_vpc_connection" {
 
 resource "google_apigee_organization" "my-test-org" {
   analytics_region   = "us-central1"
-  project_id         = data.google_client_config.current.project
+  project_id         = var.project_id
   authorized_network = google_compute_network.apigee_network.id
   depends_on         = [google_service_networking_connection.apigee_vpc_connection]
 }
