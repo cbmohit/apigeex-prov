1ccb8955adf4a9acba09acd73ebeeb6c78b7e151
cbmohit <cbmohit@gmail.com>
2024-09-01 22:54:04 +0530

    8 commit

--- main.tf
+++ main.tf
@@ -4,8 +4,27 @@ provider "google" {
   region  = var.region
 }
 
-# Enable the Cloud Resource Manager API
-resource "google_project_service" "gcp_resource_manager_api" {
-  project = var.project_id
-  service = "cloudresourcemanager.googleapis.com"
+resource "google_compute_network" "apigee_network" {
+  name       = "apigee-network"
+}
+
+resource "google_compute_global_address" "apigee_range" {
+  name          = "apigee-range"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.apigee_network.id
+}
+
+resource "google_service_networking_connection" "apigee_vpc_connection" {
+  network                 = google_compute_network.apigee_network.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.apigee_range.name]
+}
+
+resource "google_apigee_organization" "my-test-org" {
+  analytics_region   = "us-central1"
+  project_id         = var.project_id
+  authorized_network = google_compute_network.apigee_network.id
+  depends_on         = [google_service_networking_connection.apigee_vpc_connection]
 }
