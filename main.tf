f4d05dcde971c85db323356829f9cfe7140dcd4e
cbmohit <cbmohit@gmail.com>
2024-09-01 18:46:21 +0530

    2commit

--- main.tf
+++ main.tf
@@ -1,4 +1,22 @@
 provider "google" {
+  credentials = var.GOOGLE_CREDENTIALS
   project = var.project_id
   region  = var.region
+}
+
+resource "google_apigee_organization" "my-test-org" {
+  name         = "my-test-org"
+  display_name = "Example Organization"
+}
+
+resource "google_apigee_environment" "my-test-env" {
+  org_id      = google_apigee_organization."my-test-org.name
+  name        = "test"
+  display_name = "Test Environment"
+}
+
+resource "google_apigee_environment_group" "env-group" {
+  name           = "test-env-group"
+  display_name    = "Example Environment Group"
+  environments    = [google_apigee_environment.my-test-env.name]
 }
